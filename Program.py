44562d00e9f800624b9d9df7de5983ae8432fe7b
Salvakiya <garrett_roberts@hotmail.com>
2014-05-22 00:08:51 -0700

    this is a commit

--- Program.py
+++ Program.py
@@ -2,7 +2,6 @@ __author__ = 'Salvakiya'
 import configparser
 import glob
 import os
-import Traits
 
 
 def printred(string):
@@ -13,18 +12,39 @@ def printgrn(string):
     print("\033[1;36m{0}\033[00m".format(string))
 
 
-def load_file_list(file, section, option, file_type):
-    # Loads a list of files from a directory path in given ini
-    config = configparser.RawConfigParser()
-    config.read(file)
-    file_list = []
+#retrieve which mod we are currently using
+config = configparser.RawConfigParser()
+if os.path.isfile("Settings.ini"):
+    config.read("Settings.ini")
+    GAME_MOD = config.get("Options", "mod")
+else:
+    printred("Cannot find Settings.ini!")
 
-    for count in range(20):  # I assume that you wont have more than 20 directories. because that stupid
-        if config.has_option(section, option + str(count)):
-            rules_dir_reference = config.get(section, option + str(count))
-            directory = glob.glob(os.path.join(rules_dir_reference, "*"+file_type))
-            file_list.extend(directory)
-        else:
-            break
+
+#load a list of files of given type from Assets.txt
+def load_file_list(file_type):
+
+    path = "mods/"+GAME_MOD+"/"
+    file_object = open(path+"Assets.txt", 'r')
+    directory_list = []
+
+    for directory in file_object.readlines():
+        append_path = path+directory.rstrip('\n')
+        if not directory.startswith("#") and os.path.isdir(append_path):
+            directory_list.append(append_path)
+
+    file_list = []
+    for directory in directory_list:
+        printgrn(directory+"/*"+file_type)
+        files = glob.glob(directory+"/*"+file_type)
+        file_list.extend(files)
 
     return file_list
+
+
+def load_assets():
+    actor_file_list = load_file_list(".ini")
+    print(actor_file_list)
+
+
+load_assets()
